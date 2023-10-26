0598c75cf44360e346b25ad079abf8f0e5d85d8b
es0215 <77366987+es0215@users.noreply.github.com>
2023-10-26 14:54:20 +0900

    Add

---
+++ terraform/modules/google_cloud/gcs_object/variables.tf
@@ -0,0 +1,14 @@
+variable "file_name" {
+  type    = string
+  default = null
+}
+
+variable "source_file_path" {
+  type    = string
+  default = null
+}
+
+variable "bucket" {
+  type    = string
+  default = null
+}
